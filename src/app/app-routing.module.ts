1ca3fe44168a294590eb2434c36a0bbcb3ed045d
JenaniRagavan <rnjenani003@gmail.com>
2024-10-13 19:33:18 +0530

    fix:added set logo and button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './budget-planner/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    // lazy loading 
+    path: 'budget-planner',
+    loadChildren: () => import('./budget-planner/budget-planner.module').then(m => m.BudgetPlannerModule)
+  },
+  // {
+  //   path:'',
+  //   component:ProfileComponent
+  // }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
