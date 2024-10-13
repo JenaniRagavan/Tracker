ee9df362519c5b90d65acf7a7b298576baacb402
JenaniRagavan <rnjenani003@gmail.com>
2024-10-13 19:33:54 +0530

    fix:added set naviagtion

--- src/app/budget-planner/budget-planner.module.ts
+++ src/app/budget-planner/budget-planner.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BudgetPlannerRoutingModule } from './budget-planner-routing.module';
-import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+
 
 
 @NgModule({
   declarations: [
-    LoginComponent
-  ],
+    ProfileComponent
+  ],  // i remove login component from it 
   imports: [
     CommonModule,
     BudgetPlannerRoutingModule
