b8eb48b5035b584f727667b7b412b7f5f44a65f7
JenaniRagavan <rnjenani003@gmail.com>
2024-10-13 19:59:12 +0530

    fix:added remove a declarations

--- src/app/budget-planner/budget-planner.module.ts
+++ src/app/budget-planner/budget-planner.module.ts
@@ -1,17 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { BudgetPlannerRoutingModule } from './budget-planner-routing.module';
-import { TodoTranscationsComponent } from './todo-transcations/todo-transcations.component';
-
-
-
 
 @NgModule({
-  declarations: [
-  
-    TodoTranscationsComponent
-  ],  // i remove login component from it 1.login 2.profile
+  declarations: [ ],  // i remove login component from it 1.login 2.profile 3.todotranscation
   imports: [
     CommonModule,
     BudgetPlannerRoutingModule
