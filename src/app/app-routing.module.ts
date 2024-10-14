37b4476dbab125cf8afb54f023151c914e3f61aa
jenani <rnjenani003@gmail.com>
2024-10-14 11:15:21 +0530

    fix:remove modules from declarations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProfileComponent } from './budget-planner/profile/profile.component';
+import { LandingPageComponent } from './budget-planner/landing-page/landing-page.component';
 
 const routes: Routes = [
+  {
+    path:'',
+    component:LandingPageComponent
+  },
   {
     // lazy loading 
     path: 'budget-planner',
     loadChildren: () => import('./budget-planner/budget-planner.module').then(m => m.BudgetPlannerModule)
   },
-  {
-    path:'',
-    component:ProfileComponent
-  }
+  
 ];
 
 @NgModule({
